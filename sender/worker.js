51a2aabc1346c7d246edf55b1e9ff3efa7bc98be
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2017-05-07 19:25:59 -0400

    better worker

--- sender/worker.js
+++ sender/worker.js
@@ -31,8 +31,8 @@ class Worker extends EventEmitter {
 
         this.on(INIT, (data) => {
             // data is expected to be null.
-            log.info('received an init message from server');
-            log.info('asking server to get snapshot list for %s', this.fs.name);
+            log.info('received an init acknowledge from server');
+            log.info('requesting server to get zfs snapshot list for %s', this.fs.name);
             this.client.write(message(GET_SNAPSHOT_LIST, this.fs));
         })
 
@@ -56,7 +56,7 @@ class Worker extends EventEmitter {
                     log.error('please create an inital snapshot the filesystem');
                     this.client.end();
                 } else {
-                    log.info('found the following snapshots on my filesystem %s', this.fs.name);
+                    log.info('found snapshots on my filesystem %s', this.fs.name);
                     log.info(fs.list.join(', '));
 
                     // finding a common snapshots
@@ -70,36 +70,36 @@ class Worker extends EventEmitter {
 
                     if (commonList.length === 0) {
                         // initial seed to server needed.
-                        log.info('an initial seed needed');
-                        log.info('initial snapshot: %s', fs.list[0]);
+                        log.info('server needs an initial seed needed');
+                        log.info('server will be receiving an initial snapshot: %s', fs.list[0]);
                         let recvFs = {
                             name: this.fs.name,
                             incremental: false,
                             snapInitial: [fs.list[0]]
                         };
 
-                        log.info('asking the server to start up a recv for %s', recvFs.name);
+                        log.info('requesting the server to start up a zfs recv for %s', recvFs.name);
                         this.client.write(message(RECV_START, recvFs));
 
                     } else if (commonList.length === fs.list.length) {
                         // the remote list has everything in my snapshot list
-                        log.info('the server has all my snapshots');
+                        log.info('the server has all my snapshots, synchronization is complete');
                         log.info('my snapshots: %s', fs.list.join(', '));
                         log.info('server snapshots: %s', remote.list.join(', '));
 
-                        log.info('asking server to end connection');
+                        log.info('requesting server to end connection');
                         this.client.write(message(END, null));
                     } else {
                         // incremental snapshot needed, find the oldest common snapshot
-                        log.info('an incremental send is needed');
-                        log.info('from: %s to: %s', commonList[commonList.length - 1], fs.list[commonList.length]);
+                        log.info('server needs an incremental send')
+                        log.info('server will be receiving a snapshot from: %s to: %s', commonList[commonList.length - 1], fs.list[commonList.length]);
                         let recvFs = {
                             name: this.fs.name,
                             incremental: true,
                             snapFrom: commonList[commonList.length - 1],
                             snapTo: fs.list[commonList.length]
                         };
-                        log.info('asking the server to start up a recv for %s', recvFs.name);
+                        log.info('requesting the server to start up a zfs recv for %s', recvFs.name);
                         log.info(JSON.stringify(recvFs));
                         this.client.write(message(RECV_START, recvFs));
                     }
@@ -109,11 +109,11 @@ class Worker extends EventEmitter {
 
         this.on(RECV_DONE, (recvFs) => {
             if (recvFs.incremental) {
-                log.info('server said it successfully received incremental snapshot into %s', recvFs.name);
-                log.info('incremental snapshot: %s - %s', recvFs.snapFrom, recvFs.snapTo);
+                log.info('server notified successful receipt of incremental snapshot into %s', recvFs.name);
+                log.info('server received incremental snapshot from: %s to: %s', recvFs.snapFrom, recvFs.snapTo);
             } else {
-                log.info('server said it successfully received snapshot into %s', recvFs.name);
-                log.info('initial snapshot: %s', recvFs.snapInitial);
+                log.info('server notified successful receipt of initial snapshot into %s', recvFs.name);
+                log.info('server received initial snapshot: %s', recvFs.snapInitial);
             }
             log.info('sending init message to server');
             this.client.write(message(INIT, null));
