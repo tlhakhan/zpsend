232f1283d1a53f4178b8925ae2bc54a7bdb51f30
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2017-07-22 10:14:10 -0400

    missing ,

--- asker/index.js
+++ asker/index.js
@@ -8,7 +8,7 @@ let host = process.env.ZHOST || '127.0.0.1';
 let port = process.env.ZPORT || 6830;
 
 let filesystem = {
-    local: process.env.LOCALFS || 'zones/test'
+    local: process.env.LOCALFS || 'zones/test',
     remote: process.env.REMOTEFS || 'zones/test'
 
 }
