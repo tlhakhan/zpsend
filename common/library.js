3639757fa1f6d608719d49e664d98c061e1c8730
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2017-07-22 11:25:56 -0400

    map returns an array, foreach doesn't

--- common/library.js
+++ common/library.js
@@ -29,7 +29,7 @@ function getSnapshotList(fs, cb) {
         } else {
             out = out.join();
             out = out.split(/\n/);
-            out = out.forEach((item) => item.split('@')[1])
+            out = out.map(item => item.split(/@/)[1])
             cb(out);
         }
     });
